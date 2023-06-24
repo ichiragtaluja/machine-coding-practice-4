1d244a929646880c03ea9611c70783a47509619b
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-06-24 22:28:19 +0530

    style : add comments style

--- src/pages/PostDetail/PostDetail.jsx
+++ src/pages/PostDetail/PostDetail.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { useData } from "../../contexts/DataContext";
 import { Post } from "../../components/Post/Post";
 import { Link } from "react-router-dom";
+import { getTimeDifference } from "../../helpers/date";
+import "./PostDetail.css"
 
 export const PostDetail = () => {
   const { sortedForumData } = useData();
@@ -21,10 +23,23 @@ export const PostDetail = () => {
       <div className="comments-section">
         {selectedPost?.comments?.map(
           ({ commentId, username, picUrl, comment, createdAt }) => (
-            <div key={commentId}>
-              <div>
-                <img className="profile-pic" src={picUrl} />
-                <span>{comment}</span>
+            <div>
+              <div className="comment-body" key={commentId}>
+                <div>
+                  <img className="profile-pic" src={picUrl} />
+                </div>
+                <div>
+                  <span className="name">{username}</span>
+                  <span className="username">@{username}</span>{" "}
+                  <span className="username">
+                    ...{getTimeDifference(createdAt)}
+                  </span>
+                  <div>
+                    <span className="username">Replying to </span>
+                    <span className="color">@{sortedForumData.username}</span>
+                  </div>
+                  <p>{comment}</p>
+                </div>
               </div>
             </div>
           )
@@ -32,4 +47,29 @@ export const PostDetail = () => {
       </div>
     </div>
   );
+
+  // {
+  //     postId: "36635-787-65778",
+  //     username: "tanaypratap",
+  //     name: "Tanay Pratap",
+  //     picUrl: "http://bit.ly/42Zm7tM",
+  //     post: "Join InvactHQ for MBA",
+  //     postDescription:
+  //       "Non programmers on my timeline. Attention! After placing 100+ programmers i in top Indian startups, I am thinking of coming up with a program for business roles as well. Interested in helping me build this course? Join me at Invact.",
+  //     upvotes: 350,
+  //     downvotes: 50,
+  //     tags: ["mba", "business", "bootcamp"],
+  //     createdAt: "2023-06-24T12:00:00Z",
+  //     comments: [
+  //       {
+  //         commentId: "89898-856-87576",
+  //         username: "ashwin4real",
+  //         picUrl: "http://bit.ly/42Zm7tM",
+  //         likes: 0,
+  //         comment: "Interested",
+  //         createdAt: "2023-06-24T12:01:00Z",
+  //       },
+  //     ],
+  //     isBookmarked: false,
+  //   },
 };
